c95337631eaf134b65d0d2cc99e55d1f301b89d6
Saurabh Sharma <saurabhshcs@gmail.com>
2021-01-26 22:01:37 +0000

    Initial schema commit

--- src/main/java/com/techsharezone/bootify/graphql/GraphQLEndpoint.java
+++ src/main/java/com/techsharezone/bootify/graphql/GraphQLEndpoint.java
@@ -7,16 +7,11 @@ package com.techsharezone.bootify.graphql;
  */
 
 import javax.servlet.annotation.WebServlet;
-//import graphql.servlet.SimpleGraphQLServlet;
 
 
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint {
 
     public GraphQLEndpoint() {
-//        super(SchemaParser.newParser()
-//                .file("schema.graphqls") //parse the schema file created earlier
-//                .build()
-//                .makeExecutableSchema());
     }
 }
